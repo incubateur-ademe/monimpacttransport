578c234ca8da85b393f4b8875c8948edb4406651
Florian <hi@florianpanchout.com>
2021-03-16 18:58:36 +0100

    Issue 39

--- src/components/misc/configurator/transportationList/transportation/Visible.js
+++ src/components/misc/configurator/transportationList/transportation/Visible.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
+import ReactTooltip from 'react-tooltip'
 
 const Wrapper = styled.svg`
   width: 1.75rem;
@@ -19,17 +20,24 @@ const Wrapper = styled.svg`
 `
 export default function Visible(props) {
   return (
-    <Wrapper
-      x='0px'
-      y='0px'
-      width='932.15px'
-      height='932.15px'
-      viewBox='0 0 932.15 932.15'
-      checked={props.checked}
-      onClick={props.onClick}
-    >
-      <path d='M466.075,161.525c-205.6,0-382.8,121.2-464.2,296.1c-2.5,5.3-2.5,11.5,0,16.9c81.4,174.899,258.601,296.1,464.2,296.1   s382.8-121.2,464.2-296.1c2.5-5.3,2.5-11.5,0-16.9C848.875,282.725,671.675,161.525,466.075,161.525z M466.075,676.226   c-116.1,0-210.1-94.101-210.1-210.101c0-116.1,94.1-210.1,210.1-210.1c116.1,0,210.1,94.1,210.1,210.1   S582.075,676.226,466.075,676.226z' />
-      <circle cx='466.075' cy='466.025' r='134.5' />
-    </Wrapper>
+    <>
+      <Wrapper
+        data-tip={`${
+          props.checked ? 'Cacher' : 'Afficher'
+        } le mode de transport`}
+        data-multiline={true}
+        x='0px'
+        y='0px'
+        width='932.15px'
+        height='932.15px'
+        viewBox='0 0 932.15 932.15'
+        checked={props.checked}
+        onClick={props.onClick}
+      >
+        <path d='M466.075,161.525c-205.6,0-382.8,121.2-464.2,296.1c-2.5,5.3-2.5,11.5,0,16.9c81.4,174.899,258.601,296.1,464.2,296.1   s382.8-121.2,464.2-296.1c2.5-5.3,2.5-11.5,0-16.9C848.875,282.725,671.675,161.525,466.075,161.525z M466.075,676.226   c-116.1,0-210.1-94.101-210.1-210.101c0-116.1,94.1-210.1,210.1-210.1c116.1,0,210.1,94.1,210.1,210.1   S582.075,676.226,466.075,676.226z' />
+        <circle cx='466.075' cy='466.025' r='134.5' />
+      </Wrapper>
+      <ReactTooltip />
+    </>
   )
 }
