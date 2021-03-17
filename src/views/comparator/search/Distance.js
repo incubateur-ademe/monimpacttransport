f5d4d225295059ea48053b4b25077932edb5aceb
Florian <hi@florianpanchout.com>
2021-03-17 16:06:18 +0100

    Fix

--- src/views/comparator/search/Distance.js
+++ src/views/comparator/search/Distance.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from 'react'
+import React, { useContext } from 'react'
 import styled from 'styled-components'
 
 import SearchContext from 'utils/SearchContext'
