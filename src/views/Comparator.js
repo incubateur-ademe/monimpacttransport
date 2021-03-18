de523791f6ad2500609c9357093f2e1bc515d761
Florian <hi@florianpanchout.com>
2021-03-18 15:23:09 +0100

    Issue 46

--- src/views/Comparator.js
+++ src/views/Comparator.js
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react'
 import styled from 'styled-components'
 
+import SearchContext from 'utils/SearchContext'
 import ModalContext from 'utils/ModalContext'
 import Button from 'components/base/Button'
 import Search from './comparator/Search'
@@ -15,7 +16,14 @@ const Disclaimer = styled.p`
   max-width: 30rem;
   margin: 0 auto 1rem;
 `
+const Toggle = styled.span`
+  color: ${(props) => props.theme.colors.main};
+  text-decoration: underline;
+  cursor: pointer;
+`
 export default function Comparator(props) {
+  const { setMode } = useContext(SearchContext)
+
   const { setConfigurator } = useContext(ModalContext)
 
   return (
@@ -41,8 +49,25 @@ export default function Comparator(props) {
         n'est pas incluse.
       </Disclaimer>
       <Disclaimer>
-        Les distances données par le mode itinéraire sont calculées à vol
-        d'oiseau et ne correspondent pas à la réalité des transports.
+        Lorsque vous ne connaissez pas directement le kilométrage, le{' '}
+        <Toggle
+          onClick={() => {
+            window.scrollTo({
+              top: 0,
+              left: 0,
+              behavior: 'smooth',
+            })
+            setMode('itinerary')
+            window._paq &&
+              window._paq.push(['trackEvent', 'mode', 'change', 'itinerary'])
+          }}
+        >
+          mode itinéraire
+        </Toggle>{' '}
+        vous permet d'estimer la distance entre deux points. Calculée à vol
+        d'oiseau, cette distance est identique pour tous les modes de
+        déplacement et ne correspond pas à la réalité des transports disponibles
+        sur ce trajet.
       </Disclaimer>
     </>
   )
