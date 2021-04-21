65c29232645b7e8b5fc97c1777d1e158f0214497
Florian <hi@florianpanchout.com>
2021-04-21 18:13:35 +0200

    Fix Tooltip

--- src/components/misc/configurator/ButtonClose.js
+++ src/components/misc/configurator/ButtonClose.js
@@ -59,6 +59,7 @@ export default function ButtonClose(props) {
         open={props.open}
         onClick={props.onClick}
         data-tip={'Voir tous les modes de transport'}
+        data-for={'close'}
       >
         <Settings
           open={props.open}
@@ -79,7 +80,7 @@ export default function ButtonClose(props) {
           <path d='M14.757,46.02c-1.412,0-2.825-0.521-3.929-1.569c-2.282-2.17-2.373-5.78-0.204-8.063l12.758-13.418L10.637,9.645    C8.46,7.37,8.54,3.76,10.816,1.582c2.277-2.178,5.886-2.097,8.063,0.179l16.505,17.253c2.104,2.2,2.108,5.665,0.013,7.872    L18.893,44.247C17.77,45.424,16.267,46.02,14.757,46.02z' />
         </Arrow>
       </Wrapper>
-      <ReactTooltip place='right' />
+      <ReactTooltip place='right' id='close' />
     </>
   )
 }
