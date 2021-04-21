98fc90ce7bc6fffb4a0989d53b1d9697e1338c02
Florian <hi@florianpanchout.com>
2021-04-21 17:35:20 +0200

    Sources

--- src/components/wrappers/ContactWrapper.js
+++ src/components/wrappers/ContactWrapper.js
@@ -7,12 +7,14 @@ export default function ContactWrapper() {
     <Contact
       options={[
         {
-          value: 'Produit Manquant',
-          label: 'Il manque un fruit ou un légume',
+          value: 'Transport Manquant',
+          label: 'Il manque un mode de transport',
+          disclaimer: `Ce simulateur n'est qu'une illustration des différences entre les principaux modes de transport et n'a pas pour objectif d'être exhaustif.`,
         },
         {
           value: 'Imprecision',
-          label: `Les chiffres ne sont pas bon`,
+          label: `Le calcul n'est pas assez précis`,
+          disclaimer: `Ce simulateur propose un calcul simplifié afin de donner une idée d'ordre de grandeur. Réalisez votre bilan carbone sur Nos Gestes Climat afin d'obtenir un résultat plus précis.`,
         },
       ]}
     />
--- src/components/wrappers/FooterWrapper.js
+++ src/components/wrappers/FooterWrapper.js
@@ -1,22 +1,49 @@
 import React from 'react'
 
 import MagicLink from 'components/base/MagicLink'
+import Emoji from 'components/base/Emoji'
 import Footer from 'components/layout/Footer'
 
 export default function FooterWrapper() {
   return (
-    <Footer background={'background'}>
+    <Footer>
       <h2>D'ou viennent ces données ?</h2>
       <p>
-        Ce simulateur réutilise les données du{' '}
-        <MagicLink to='https://www.ademe.fr/calendrier-fruits-legumes-saison'>
-          calendrier des fruits et légumes de saison de l'ADEME
-        </MagicLink>{' '}
-        et de la base de donnée{' '}
-        <MagicLink to='https://agribalyse.ademe.fr/'>Agribalyse</MagicLink>.
-        Agribalyse est la base de données de référence des indicateurs d'impacts
-        environnementaux des produits agricoles produits en France et des
-        produits alimentaires consommés en France.
+        Ce simulateur réutilise les données de la{' '}
+        <MagicLink to='https://www.bilans-ges.ademe.fr/fr/accueil/contenu/index/page/presentation/siGras/0'>
+          Base Carbone
+        </MagicLink>
+        . Il s’agit d’une{' '}
+        <strong>base de données publique de facteurs d'émissions</strong>,
+        nécessaires à la réalisation d’un bilan d’émissions de gaz à effet de
+        serre (GES) et plus généralement tout exercice de comptabilité carbone.
+      </p>
+      <p>
+        <strong>Équivalent CO₂ par personne en France.</strong>
+      </p>
+      <p>
+        <Emoji>⚠️</Emoji>{' '}
+        <strong>
+          Sont inclus les émissions directes, l'entretien du véhicule, et la
+          production et distribution de carburant et d'électricité.
+        </strong>
+        .
+      </p>
+      <p>
+        <Emoji>⚠️</Emoji> <strong>La construction des véhicules</strong>{' '}
+        (voiture, vélo, batterie, train, avion...){' '}
+        <strong>et des infrastructures</strong> (routes, rails, aéroports...){' '}
+        <strong>n'est pas incluse.</strong>
+      </p>
+      <p>
+        Si vous souhaitez aller plus loin dans votre démarche, vous pouvez{' '}
+        <strong>
+          calculer votre empreinte sur le climat grace à notre simulateur{' '}
+          <MagicLink to={'https://nosgestesclimat.fr/'}>
+            Nos Gestes Climat
+          </MagicLink>
+        </strong>
+        .
       </p>
     </Footer>
   )
