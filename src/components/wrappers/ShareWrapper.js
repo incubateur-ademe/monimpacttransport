4fdf730922eb413848ea741b8b61b74f5b340c75
Martin Régner <46482115+martinregner@users.noreply.github.com>
2021-07-12 12:06:50 +0200

    initialisation partage

--- src/components/wrappers/ShareWrapper.js
+++ src/components/wrappers/ShareWrapper.js
@@ -6,8 +6,8 @@ export default function ShareWrapper(props) {
   return (
     <Share
       small={props.small}
-      title='Mes Fruits et Légumes de Saison'
-      message={`Découvrez quels sont les fruits et légumes de saison pour consommer des produits frais et locaux toute l'année ! En plus c'est meilleur pour votre santé et pour l'environnement !`}
+      title='Mon Impact Transport'
+      message={`Découvrez et comparez facilement l'impact des déplacements sur l'environnement !`}
     />
   )
 }
