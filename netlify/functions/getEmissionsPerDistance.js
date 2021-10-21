fe6f837e0d827ad561e14ba6a525cf1cc7d8de1b
Florian <hi@florianpanchout.com>
2021-10-21 16:13:45 +0200

    Fix CORS

--- netlify/functions/getEmissionsPerDistance.js
+++ netlify/functions/getEmissionsPerDistance.js
@@ -25,6 +25,11 @@ exports.handler = async function (event) {
 
   return {
     statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Headers': 'Content-Type',
+      'Access-Control-Allow-Methods': 'GET, POST, OPTION',
+    },
     body: JSON.stringify(
       transportations
         // Remove transportations without data
