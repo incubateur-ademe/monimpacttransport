2d8985ae5d12830b1d6b410bcfe85262054d1ee6
Florian <hi@florianpanchout.com>
2022-01-21 11:16:44 +0100

    Issue 91

--- src/components/modals/OccupancyModal.js
+++ src/components/modals/OccupancyModal.js
@@ -3,6 +3,7 @@ import styled from 'styled-components'
 
 import ModalContext from 'utils/ModalContext'
 import Modal from 'components/base/Modal'
+import MagicLink from 'components/base/MagicLink'
 
 const Title = styled.h2``
 const Text = styled.p``
@@ -16,9 +17,15 @@ export default function Occupancy() {
         l'ensemble du véhicule.
       </Text>
       <Text>
-        Nous prenons les taux de remplissage moyen préconisés par l'ADEME, sauf
-        pour les voitures (thermique et électrique) ou nous ne comptons qu'une
-        seule personne dans le véhicule.
+        Les taux de remplissage utilisés dans Mon Impact Transport sont ceux
+        retenus dans la{' '}
+        <MagicLink to='https://bilans-ges.ademe.fr/documentation/UPLOAD_DOC_FR/index.htm?transport_de_personnes.htm'>
+          Base Carbone de l'ADEME
+        </MagicLink>
+        , sauf pour les voitures (thermiques et électriques) pour lesquelles
+        nous ne comptons qu'une seule personne dans le véhicule. Il est possible
+        de moduler le nombre de personnes par voiture avec l'option "Afficher le
+        covoiturage".
       </Text>
     </Modal>
   )
