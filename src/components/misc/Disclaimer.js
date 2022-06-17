5a2a16d2fbce05d60ffd87da3d69e2493c874fb3
Florian <hi@florianpanchout.com>
2022-06-17 13:34:50 +0200

    Add disclaimer

--- src/components/misc/Disclaimer.js
+++ src/components/misc/Disclaimer.js
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import styled from 'styled-components'
 
 import useIframe from 'hooks/useIframe'
+import TransportationContext from 'utils/TransportationContext'
 
 const Wrapper = styled.div`
   margin-top: 3rem;
@@ -12,6 +13,8 @@ const Wrapper = styled.div`
 export default function Disclaimer(props) {
   const iframe = useIframe()
 
+  const { construction } = useContext(TransportationContext)
+
   return (
     <Wrapper iframe={iframe}>
       {props.itinerary && (
@@ -20,12 +23,26 @@ export default function Disclaimer(props) {
           ne correspondent pas forcement à la réalité.
         </p>
       )}
+      {construction && !props.itinerary && (
+        <p>
+          Seul les véhicules dont l'impact de la construction sont connus sont
+          affichés
+        </p>
+      )}
       <p>
         <strong>Équivalent CO₂ par personne en France.</strong> Sont incluses
         les émissions directes, et la production et distribution de carburant et
-        d'électricité. <strong>La construction des véhicules</strong> (voiture,
-        vélo, batterie, train, avion...) <strong>et des infrastructures</strong>{' '}
-        (routes, rails, aéroports...) <strong>n'est pas incluse.</strong>
+        d'électricité. <strong>La construction des</strong>
+        {construction ? (
+          ''
+        ) : (
+          <>
+            <strong> véhicules</strong> (voiture, vélo, batterie, train,
+            avion...) <strong>et des</strong>
+          </>
+        )}
+        <strong> infrastructures</strong> (routes, rails, aéroports...){' '}
+        <strong>n'est pas incluse.</strong>
       </p>
     </Wrapper>
   )
