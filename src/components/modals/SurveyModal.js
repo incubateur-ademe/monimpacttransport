64468dc3cd45fdcb4673cde98169cedb40b918a7
Florian <hi@florianpanchout.com>
2022-09-16 10:53:31 +0200

    Fix iframe load

--- src/components/modals/SurveyModal.js
+++ src/components/modals/SurveyModal.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useState, useEffect, useContext } from 'react'
 import styled from 'styled-components'
 
 import useIframe from 'hooks/useIframe'
@@ -11,21 +11,31 @@ const StyledModal = styled(Modal)`
 export default function SurveyModal() {
   const { survey: open, setSurvey: setOpen } = useContext(ModalContext)
 
-  const iframe = useIframe()
+  const isIframe = useIframe()
+  const [iframe, setIframe] = useState(false)
+  useEffect(() => {
+    const timer = setTimeout(() => setIframe(true), 2500)
+
+    return () => {
+      clearTimeout(timer)
+    }
+  }, [])
 
   return (
     <StyledModal open={open} setOpen={setOpen}>
-      <iframe
-        title='enquete'
-        src={
-          iframe
-            ? 'https://airtable.com/embed/shr6mPp207MDByuI4?backgroundColor=teal'
-            : 'https://airtable.com/embed/shrHHi3KASi7cDNJl?backgroundColor=teal'
-        }
-        frameBorder='0'
-        width='100%'
-        height='533'
-      ></iframe>
+      {open || iframe ? (
+        <iframe
+          title='enquete'
+          src={
+            isIframe
+              ? 'https://airtable.com/embed/shr6mPp207MDByuI4?backgroundColor=teal'
+              : 'https://airtable.com/embed/shrHHi3KASi7cDNJl?backgroundColor=teal'
+          }
+          frameBorder='0'
+          width='100%'
+          height='533'
+        ></iframe>
+      ) : null}
     </StyledModal>
   )
 }
