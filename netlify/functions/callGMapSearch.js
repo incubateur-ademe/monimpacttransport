8bfa17fcf20f4b7ae841d9d7bdab786750a4e225
Florian <hi@florianpanchout.com>
2022-09-20 14:11:05 +0200

    Fix call

--- netlify/functions/callGMapSearch.js
+++ netlify/functions/callGMapSearch.js
@@ -5,11 +5,11 @@ const countries = require('./countries.json')
 exports.handler = function (event) {
   const country = countries[event.headers['x-country']]
   console.log(
-    `https://photon.komoot.io/api/?q=${event.rawQuery}&lat=${country[0]}&lon${country[1]}&lang=fr`
+    `https://photon.komoot.io/api/?q=${event.rawQuery}&lat=${country[0]}&lon=${country[1]}&lang=fr`
   )
   return axios
     .get(
-      `https://photon.komoot.io/api/?q=${event.rawQuery}&lat=${country[0]}&lon${country[1]}&lang=fr`
+      `https://photon.komoot.io/api/?q=${event.rawQuery}&lat=${country[0]}&lon=${country[1]}&lang=fr`
     )
     .then((res) => ({
       statusCode: 200,
